dc76699baccb8d728bdf2c4b27df68e378025e0e
Martin Hellmich <mhellmic@cern.ch>
2013-07-25 14:33:33 +0200

    send exemplary transfer request

--- static/scripts/transfer.js
+++ static/scripts/transfer.js
@@ -23,7 +23,41 @@ define([
       });
       console.log(filelist);
       // create fts request
+      /// get source filenames
+      var filenames = filelist.map(function(file) { return file.get('name'); });
+      /// get source paths
+      
+      ///  get dest path equivalents
+
+      var data = 
+      { "files": [
+          {
+            "sources": filenames,
+            "destinations": ["root:"],
+            "metadata": "User defined metadata",   
+            "filesize": 1024,                      
+            "checksum": 'adler32:1234',            
+          }
+        ], 
+        "params": {
+          "verify_checksum": true,    
+          "reuse": false,             
+          "spacetoken": null,         
+          "bring_online": null,
+          "copy_pin_lifetime": -1,    
+          "job_metadata": null,       
+          "source_spacetoken": null,  
+          "overwrite": false,         
+          "gridftp": null             
+        }
+      };
+
       // send request
+      $.post("https://fts3-pilot.cern.ch:8446/jobs",
+      data, function(data, status, xhr) {
+      console.log(data);
+      console.log(status)
+      }, "application/json");
     }
   });
 
